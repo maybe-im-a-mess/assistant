e49ca227ab3e40947416f21b37f886a3f7d9c4fb
maybe-im-a-mess <a.wonder467@gmail.com>
2021-11-16 15:17:52 +0100

    i want to die 16.11.2021

--- actions/actions.py
+++ actions/actions.py
@@ -6,26 +6,26 @@
 
 
 # This is a simple example for a custom action which utters "Hello World!"
+
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 import requests
 import requests as requests
 
-#x = requests.get('')
-#print(x.text)
 
+# x = requests.get('')
+# print(x.text)
 
 
 class Greeting(Action):
+    def name(self) -> Text:
+        return "say_hello"
 
-     def name(self) -> Text:
-         return "say_hallo"
-
-     def run(self, dispatcher: CollectingDispatcher,
-             tracker: Tracker,
-             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        dispatcher.utter_greet()
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_greet("Hello")
 
         return []
+
