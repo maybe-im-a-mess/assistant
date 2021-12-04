514c205de901c16d86c5bf945665a9c09c40fee8
maybe-im-a-mess <a.wonder467@gmail.com>
2021-12-04 23:33:29 +0100

    PARSER FOR PROFESSORS AND THEIR ROOMS! (Zhenya has nothing to do with this)

--- app.py
+++ app.py
@@ -6,19 +6,25 @@ from bs4 import BeautifulSoup as BS
 r = requests.get("https://www.th-deg.de/de/hochschule/kontakt/fakultaet-ai")
 html = BS(r.content, 'html.parser')
 
-
 app = Flask(__name__)
 
 persons = []
 
-i = 1
-for el in html.select(".referat_row > .moreMitarbeiter"):
-    name = el.select(".text-overlay > .label_name")
-    # room = el.select(".text-overlay > .fa-map-marker-alt")
-    # element = {'id': i, 'name': name[0].text, 'room': room[0].text}
-    #persons.append(element)
-    i += 1
+infos = []
 
+for el in html.select(".referat_row > .moreMitarbeiter"):
+    for para in el.find_all("p"):
+        infos.append(para.get_text())
+
+profs = [infos[i:i + 5]
+         for i in range(0, len(infos), 5)]
+
+for i in range(len(profs)):
+    profs[i].pop(1)
+    profs[i].pop(2)
+    profs[i].pop(2)
+    element = {'id': i + 1, 'name': profs[i][0], 'room': profs[i][1]}
+    persons.append(element)
 
 print(persons)
 
@@ -26,9 +32,11 @@ print(persons)
 def _find_next_id():
     return max(person["id"] for person in persons) + 1
 
+
 @app.get("/countries")
 def get_countries():
     return jsonify(persons)
+
 
 @app.post("/countries")
 def add_country():
